98ab9b0f3f25ad1e0077a4204924048095304fbc
Martyn_Jones <Martyn.Jones@nuance.com>
2019-03-30 11:05:02 +0000

    correct js code

--- visits/index.js
+++ visits/index.js
@@ -9,12 +9,12 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    client.get ('vists', (err, visits) => {
-        res.send('Number of visits is ' + visits);
-        client.set('visits', parseInt(visits) + 1);
-    });
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
 });
 
 app.listen(8081, () => {
-    console.log('Listening on port 8081');
+  console.log('Listening on port 8081');
 });
